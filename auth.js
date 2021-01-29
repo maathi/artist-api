1e26c403a837a8ba2ba0085a9553f0a749f78f95
maathi <mahdi221b@gmail.com>
2021-01-29 18:25:45 +0100

    another bloody typo

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 
 function authenticate(token) {
   try {
-    let user = jwt.verify(token, "key")
+    let user = jwt.verify(token, process.env.SECRET)
     return user
   } catch (err) {
     return null
